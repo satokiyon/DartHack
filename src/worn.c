satokiyon/DartHack

commit 6fea2b758fe594e6e94c9b778a7d05bca42199ce

gold dragon and scales
Add two new monsters and two new objects:
 gold dragon
 baby gold dragon
 gold dragon scale mail
 set of gold dragon scales

A couple of variants seem to have added these already, but this came
off my ancient list of monsters to add and was done from scratch.
It's a clone of silver dragon, but instead of having reflection and
breathing cold, a gold dragon emits light and breathes fire; because
of the latter it can be seen with infravision like a red dragon.
Adult gold dragons are lawful as in the AD&D Monster Manual rather
than chaotic as the wiki pages show for the variant versions.

Worn gold dragon scales operate similar to wielded Sunsword:  when
blessed, radius is 3 (same as a lamp), if uncursed, radius is 2, and
if cursed, radius is 1 (but functions as 2 when worn by the hero,
otherwise there would be no tangible effect).  Gold dragon scale mail
gets an extra +1, making blessed gold DSM have a bigger radius than
lamps.  Embedded scales have radius 1 regardless of BUC state; light
for that case comes from the gold dragon monster form the hero is in.
When not worn, gold scales and scale-mail don't emit any light.

The tiles use a mix of yellow (for gold) and red.  The two object
tiles seem reasonable variations of the corresponding silver dragon
ones.  The two monster tiles definitely need work since the silver
ones were mostly cyan and changing that to red did not produce very
good result; subsequent attempt at a mixture was haphazard at best.

src/worn.c
@@ -528,8 +528,9 @@ m_dowear_type(struct monst *mon, long flag, boolean creation,
               boolean racialexception)
 {
     struct obj *old, *best, *obj;
+    long oldmask = 0L;
     int m_delay = 0;
-    int unseen = !canseemon(mon);
+    int sawmon = canseemon(mon), sawloc = cansee(mon->mx, mon->my);
     boolean autocurse;
     char nambuf[BUFSZ];
 
@@ -629,13 +630,15 @@ m_dowear_type(struct monst *mon, long flag, boolean creation,
         m_delay += 2;
     /* when upgrading a piece of armor, account for time spent
        taking off current one */
-    if (old)
+    if (old) {
         m_delay += objects[old->otyp].oc_delay;
 
-    if (old) /* do this first to avoid "(being worn)" */
-        old->owornmask = 0L;
+        oldmask = old->owornmask; /* needed later by artifact_light() */
+        old->owornmask = 0L; /* avoid doname() showing "(being worn)" */
+    }
+
     if (!creation) {
-        if (canseemon(mon)) {
+        if (sawmon) {
             char buf[BUFSZ];
 
             if (old)
@@ -654,20 +657,45 @@ m_dowear_type(struct monst *mon, long flag, boolean creation,
         if (mon->mfrozen)
             mon->mcanmove = 0;
     }
-    if (old)
+    if (old) {
         update_mon_intrinsics(mon, old, FALSE, creation);
+
+        /* owornmask was cleared above but artifact_light() expects it */
+        old->owornmask = oldmask;
+        if (old->lamplit && artifact_light(old))
+            end_burn(old, FALSE);
+        old->owornmask = 0L;
+    }
     mon->misc_worn_check |= flag;
     best->owornmask |= flag;
     if (autocurse)
         curse(best);
+    if (artifact_light(best) && !best->lamplit) {
+        begin_burn(best, FALSE);
+        vision_recalc(1);
+        if (!creation && best->lamplit && cansee(mon->mx, mon->my)) {
+            const char *adesc = arti_light_description(best);
+
+            if (sawmon) /* could already see monster */
+                pline("%s %s to shine %s.", Yname2(best),
+                      otense(best, "begin"), adesc);
+            else if (canseemon(mon)) /* didn't see it until new light */
+                pline("%s %s shining %s.", Yname2(best),
+                      otense(best, "are"), adesc);
+            else if (sawloc) /* saw location but not invisible monster */
+                pline("%s begins to shine %s.", Something, adesc);
+            else /* didn't see location until new light */
+                pline("%s is shining %s.", Something, adesc);
+        }
+    }
     update_mon_intrinsics(mon, best, TRUE, creation);
     /* if couldn't see it but now can, or vice versa, */
-    if (!creation && (unseen ^ !canseemon(mon))) {
+    if (!creation && (sawmon ^ !canseemon(mon))) {
         if (mon->minvis && !See_invisible) {
             pline("Suddenly you cannot see %s.", nambuf);
             makeknown(best->otyp);
-        } /* else if (!mon->minvis) pline("%s suddenly appears!",
-             Amonnam(mon)); */
+        } /* else if (!mon->minvis)
+           *     pline("%s suddenly appears!", Amonnam(mon)); */
     }
 }
 #undef RACE_EXCEPTION
@@ -1057,6 +1085,11 @@ extract_from_minvent(
         impossible("extract_from_minvent called on object not in minvent");
         return;
     }
+    /* handle gold dragon scales/scale-mail (lit when worn) before clearing
+       obj->owornmask because artifact_light() expects that to be W_ARM */
+    if ((unwornmask & W_ARM) != 0 && obj->lamplit && artifact_light(obj))
+        end_burn(obj, FALSE);
+
     obj_extract_self(obj);
     obj->owornmask = 0L;
     if (unwornmask) {

doc/fixes37.0
@@ -1013,8 +1013,10 @@ boiling a pool or fountain now creates a temporary cloud of steam
 random themed rooms in the dungeons of doom
 extended achievement and conduct fields for xlogfile
 record amount of gold in hero's possession in xlogfile
-new objects:  amulets of flying and guarding
-new monsters:  displacer beast ('f') and genetic engineer ('Q')
+new objects:  amulets of flying and guarding, gold dragon scale mail and
+	set of gold dragon scales
+new monsters:  displacer beast ('f') and genetic engineer ('Q'), gold dragon
+	and baby gold dragon (both 'D')
 make camera flash which reveals previously unseen map features or objects or
 	monsters record those on the hero's map; monsters revert to 'unseen'
 boolean options can optionally have the form "name:value" with value taken

include/extern.h
@@ -1338,6 +1338,7 @@ extern struct obj *rnd_treefruit_at(int, int);
 extern void set_corpsenm(struct obj *, int);
 extern long rider_revival_time(struct obj *, boolean);
 extern void start_corpse_timeout(struct obj *);
+extern void maybe_adjust_light(struct obj *, int);
 extern void bless(struct obj *);
 extern void unbless(struct obj *);
 extern void curse(struct obj *);

include/mondata.h
@@ -197,10 +197,15 @@
 #define emits_light(ptr)                                          \
     (((ptr)->mlet == S_LIGHT || (ptr) == &mons[PM_FLAMING_SPHERE] \
       || (ptr) == &mons[PM_SHOCKING_SPHERE]                       \
+      || (ptr) == &mons[PM_BABY_GOLD_DRAGON]                      \
       || (ptr) == &mons[PM_FIRE_VORTEX])                          \
          ? 1                                                      \
-         : ((ptr) == &mons[PM_FIRE_ELEMENTAL]) ? 1 : 0)
-/*	[note: the light ranges above were reduced to 1 for performance...] */
+         : ((ptr) == &mons[PM_FIRE_ELEMENTAL]                     \
+            || (ptr) == &mons[PM_GOLD_DRAGON]) ? 1 : 0)
+    /* [Note: the light ranges above were reduced to 1 for performance,
+     *  otherwise screen updating on the plane of fire slowed to a crawl.
+     *  Note too: that was with 1990s hardware and before fumarole smoke
+     *  blocking line of sight was added, so might no longer be necessary.] */
 #define likes_lava(ptr) \
     (ptr == &mons[PM_FIRE_ELEMENTAL] || ptr == &mons[PM_SALAMANDER])
 #define pm_invisible(ptr) \

include/patchlevel.h
@@ -17,7 +17,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 35
+#define EDITLEVEL 36
 
 /*
  * Development status possibilities.

src/artifact.c
@@ -1675,6 +1675,13 @@ finesse_ahriman(struct obj *obj)
 boolean
 artifact_light(struct obj *obj)
 {
+    /* not artifacts but treat them as if they were because they emit
+       light without burning */
+    if (obj && (obj->otyp == GOLD_DRAGON_SCALE_MAIL
+                || obj->otyp == GOLD_DRAGON_SCALES)
+        && (obj->owornmask & W_ARM) != 0L)
+        return TRUE;
+
     return (boolean) (get_artifact(obj) && obj->oartifact == ART_SUNSWORD);
 }
 

src/do_wear.c
@@ -695,21 +695,37 @@ static int
 Armor_on(void)
 {
     /*
-     * No suits require special handling.  Special properties conferred by
-     * suits are set up as intrinsics (actually 'extrinsics') by setworn()
-     * which is called by armor_or_accessory_on() before Armor_on().
+     * Gold DSM requires special handling since it emits light when worn.
      */
-    if (uarm) /* no known instances of !uarm here but play it safe */
-        uarm->known = 1; /* suit's +/- evident because of status line AC */
+    if (!uarm) /* no known instances of !uarm here but play it safe */
+        return 0;
+    uarm->known = 1; /* suit's +/- evident because of status line AC */
+
+    if (artifact_light(uarm) && !uarm->lamplit) {
+        begin_burn(uarm, FALSE);
+        if (!Blind)
+            pline("%s %s to shine %s!",
+                  Yname2(uarm), otense(uarm, "begin"),
+                  arti_light_description(uarm));
+    }
     return 0;
 }
 
 int
 Armor_off(void)
 {
+    struct obj *otmp = uarm;
+    boolean was_arti_light = otmp && otmp->lamplit && artifact_light(otmp);
+
     g.context.takeoff.mask &= ~W_ARM;
     setworn((struct obj *) 0, W_ARM);
     g.context.takeoff.cancelled_don = FALSE;
+
+    if (was_arti_light && !artifact_light(otmp)) {
+        end_burn(otmp, FALSE);
+        if (!Blind)
+            pline("%s shining.", Tobjnam(otmp, "stop"));
+    }
     return 0;
 }
 
@@ -722,9 +738,18 @@ Armor_off(void)
 int
 Armor_gone(void)
 {
+    struct obj *otmp = uarm;
+    boolean was_arti_light = otmp && otmp->lamplit && artifact_light(otmp);
+
     g.context.takeoff.mask &= ~W_ARM;
     setnotworn(uarm);
     g.context.takeoff.cancelled_don = FALSE;
+
+    if (was_arti_light && !artifact_light(otmp)) {
+        end_burn(otmp, FALSE);
+        if (!Blind)
+            pline("%s shining.", Tobjnam(otmp, "stop"));
+    }
     return 0;
 }
 
@@ -2773,6 +2798,10 @@ destroy_arm(register struct obj *atmp)
     } else if (DESTROY_ARM(uarm)) {
         if (donning(otmp))
             cancel_don();
+        /* for gold DSM, we don't want Armor_gone() to report that it
+           stops shining _after_ we've been told that it is destroyed */
+        if (otmp->lamplit)
+            end_burn(otmp, FALSE);
         Your("armor turns to dust and falls to the %s!", surface(u.ux, u.uy));
         (void) Armor_gone();
         useup(otmp);

src/light.c
@@ -775,6 +775,8 @@ candle_light_range(struct obj *obj)
 int
 arti_light_radius(struct obj *obj)
 {
+    int res;
+
     /*
      * Used by begin_burn() when setting up a new light source
      * (obj->lamplit will already be set by this point) and
@@ -789,20 +791,34 @@ arti_light_radius(struct obj *obj)
     /* cursed radius of 1 is not noticeable for an item that's
        carried by the hero but is if it's carried by a monster
        or left lit on the floor (not applicable for Sunsword) */
-    return (obj->blessed ? 3 : !obj->cursed ? 2 : 1);
+    res = (obj->blessed ? 3 : !obj->cursed ? 2 : 1);
+
+    /* if poly'd into gold dragon with embedded scales, make the scales
+       have minimum radiance (hero as light source will use light radius
+       based on monster form); otherwise, worn gold DSM gives off more
+       light than other light sources */
+    if (obj == uskin)
+        res = 1;
+    else if (obj->otyp == GOLD_DRAGON_SCALE_MAIL) /* DSM but not scales */
+        ++res;
+
+    return res;
 }
 
-/* adverb describing lit artifact's light; depends on curse/bless state */
+/* adverb describing lit artifact's light; radius varies depending upon
+   curse/bless state; also used for gold dragon scales/scale mail */
 const char *
 arti_light_description(struct obj *obj)
 {
     switch (arti_light_radius(obj)) {
+    case 4:
+        return "radiantly"; /* blessed gold dragon scale mail */
     case 3:
-        return "brilliantly"; /* blessed */
+        return "brilliantly"; /* blessed artifact, uncursed gold DSM */
     case 2:
-        return "brightly"; /* uncursed */
+        return "brightly"; /* uncursed artifact, cursed gold DSM */
     case 1:
-        return "dimly"; /* cursed */
+        return "dimly"; /* cursed artifact, embedded scales */
     default:
         break;
     }

commit 3e4a2b4546384bb277e1a1988f6459d07299bd1b

Fix: 'Suddenly you cannot see it'
Updates to m_dowear_type(worn.c) in 6fea2b758f inadvertently reversed
the effect of an xor near the end of the function, causing it to go from
testing whether a monster's status as 'seen' or 'unseen' changed over
the course of the function to testing whether it remained the same.

As a result, whenever an unseen, invisible monster donned a piece of
armor anywhere on the map, the message 'suddenly you cannot see it'
would be printed.  Fix the xor so that it goes back to testing whether
visibility changed since the start of the function.

src/worn.c
@@ -727,7 +727,7 @@ m_dowear_type(struct monst *mon, long flag, boolean creation,
     }
     update_mon_intrinsics(mon, best, TRUE, creation);
     /* if couldn't see it but now can, or vice versa, */
-    if (!creation && (sawmon ^ !canseemon(mon))) {
+    if (!creation && (sawmon ^ canseemon(mon))) {
         if (mon->minvis && !See_invisible) {
             pline("Suddenly you cannot see %s.", nambuf);
             makeknown(best->otyp);